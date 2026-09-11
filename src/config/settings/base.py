8ca2595e64c7770151ff146c77b217e919c86bd8
Dandre89 <dandre@activatedmobilesolutions.com>
2026-09-10 23:26:32 -0400

    Update base.py

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -91,7 +91,7 @@ DATABASES = {
         "PORT": os.environ.get("POSTGRES_PORT", "5432"),
         "CONN_MAX_AGE": 60,
         "OPTIONS": (
-            {"options": f"-c search_path={POSTGRES_SCHEMA},public"}
+            {"options": f"-c search_path={POSTGRES_SCHEMA}"}
             if POSTGRES_SCHEMA
             else {}
         ),
